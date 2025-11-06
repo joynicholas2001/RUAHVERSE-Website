a858f8307ca81cb7b38310d9a28411b78c28404f
JOY NICHOLAS <itzjoynicholas@gmail.com>
2025-11-06 18:02:10 +0530

    Remove Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
 import Technologies from './components/Technologies'
-import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './App.css'
@@ -18,7 +17,6 @@ function App() {
         <About />
         <Services />
         <Technologies />
-        <Projects />
         <Contact />
       </main>
       <Footer />
